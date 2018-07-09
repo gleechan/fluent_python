7944cfbea070cd88c1799adc2bfe86b1fb0f367b
chenyongjian <chenyongjian@jituia.com>
2018-07-09 17:19:08 +0800

    lesson 2

---
+++ lesson2/symbols.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright jitui 2018
+#
+# @author: chenyongjian@jituia.com
+#
+
+symbols = 'abcde'
+# for symbol in symbols:
+#     codes.append(ord(symbol))
+
+codes = [ord(s) for s in symbols]
+print(codes)
