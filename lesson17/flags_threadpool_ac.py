65dc645151288c8e71f357f5a735836c8f4a2404
gleechan <gleechan@hotmail.com>
2019-05-06 15:32:05 +0000

    fixed

---
+++ lesson17/flags_threadpool_ac.py
@@ -0,0 +1,21 @@
+from concurrent import futures
+from flags_threadpool import download_one
+
+
+def download_many(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in cc_list:
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+
+    return len(results)
